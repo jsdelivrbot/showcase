c6234841cc73239f220ff1b85ff8cffc9997fe39
Alvaro Martinez <alvaromartinez@Alvaros-MBP-2.home>
2017-12-28 10:32:37 -0500

    server set up

---
+++ portfolio/server/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false}));
+
+
+
+
+app.use(function(req, res, next) { //allow cross origin requests
+    res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+    res.header("Access-Control-Allow-Origin", "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`API running on localhost:${port} `));
